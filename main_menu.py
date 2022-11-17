fec068273a4933c239202131cf520d048a738f80
success4lyf <successanele111@gmail.com>
2022-11-17 22:51:26 +0000

    Updated code to create the main menu for the cafe app

--- main_menu.py
+++ main_menu.py
@@ -3,6 +3,7 @@
 from product_menu import ProductMenu
 from order_menu import OrderMenu
 from courier_menu import CourierMenu
+from files import FileContent
 
 class MainMenu:
     def __init__(self, exit, menu_option):
@@ -22,6 +23,11 @@ class MainMenu:
             user = int(input('Enter a main menu option: '))
             if user == 0:
                 print('\n' + f'You entered {user} -> {menu_option[0]}. \n')
+                # FileContent.get_product_file(self)
+                # FileContent.write_product(self)
+                # FileContent.get_courier_file(self)
+                # FileContent.get_orders_file(self)
+                print('file saved')
                 m_menu.exit_app()
 
             elif user == 1:
@@ -30,19 +36,19 @@ class MainMenu:
                 ProductMenu.get_product_menu(self)         
 
             elif user == 2:
-                print(f'You entered {user}. The order menu options are: ')
-                OrderMenu.get_order_menu(self)
-
+                print(f'You entered {user}. The order menu options are: ')                
+                CourierMenu.get_courier_menu(self)
+                
             elif user == 3:
                 print(f'You entered {user}. The courier menu options are: ')
-                CourierMenu.get_courier_menu(self)
+                OrderMenu.get_order_menu(self)
 
 
     def exit_app(self):
         print('You have \x1B[1m EXITED \x1B[0m the App. \n')
         quit()
 
-exit = 0
-menu_option = ['EXIT', 'PRODUCTS', 'ORDERS', 'COURIER']
+exit = 'exit'
+menu_option = ['EXIT', 'PRODUCTS', 'COURIER', 'ORDERS']
 m_menu = MainMenu(exit, menu_option)
 m_menu.get_main_menu()
