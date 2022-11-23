dcba9be0e0f469a42fbb2d9b162518140de6066d
success4lyf <successanele111@gmail.com>
2022-11-23 11:51:35 +0000

    Updated code to get data from database, insert, update and delete from the product menu options

--- product_menu.py
+++ product_menu.py
@@ -1,12 +1,34 @@
-##### MINI PROJECT WEEK 1 #####
-
 import csv
+import pymysql
+import os
+from dotenv import load_dotenv
 
 class ProductMenu():
     def __init__(self, product_menu):
         self.product_menu = product_menu
         self.products_list = []
-    
+        
+        load_dotenv()
+        host = os.environ.get("mysql_host")
+        user = os.environ.get("mysql_user")
+        password = os.environ.get("mysql_pass")
+        database = os.environ.get("mysql_db")
+
+        self.connection = pymysql.connect(
+            host,
+            user,
+            password,
+            database
+        )
+
+        self.cursor = self.connection.cursor()
+
+    def get_products_from_sql(self):
+        self.cursor.execute('SELECT * FROM products')
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(f'PRODUCT_ID: {str(row[0])}, PRODUCT_NAME: {str(row[1])}, PRICE: {str(row[2])}')
+
     def load_products(self):
         with open('file_content/product.csv', 'r') as file:
             dict_reader = csv.DictReader(file)
@@ -24,11 +46,9 @@ class ProductMenu():
         file.close()
 
     def get_product_menu(self):
-        print('\n')
-        print('*' * 5 + ' ' + 'PRODUCT MENU:' + ' ' + '*' * 5 + '\n')
+        print('\n' + '*' * 5 + ' ' + 'PRODUCT MENU:' + ' ' + '*' * 5 + '\n')
         for i, option in enumerate(product_menu):
-            print(f'{i} -> {option}')
-            print(' ') 
+            print(f'{i} -> {option} \n')
         
         user_p = int(input('Enter a product menu option: '))
         if user_p == 0:
@@ -47,7 +67,7 @@ class ProductMenu():
     def get_product_list(self):
         while True:
             print('\n' + 'The products list and prices are: \n')
-            p_menu.load_products()
+            p_menu.get_products_from_sql()
             break
         p_menu.get_product_menu()
 
@@ -55,76 +75,59 @@ class ProductMenu():
         print('Create a new product you want to add to the product list...')
         user_new = input('Enter a new product name: ')
         user_price = input('Enter the price: ')        
-        print('you created a new product - ', 'Name: ', user_new, ',' , 'Price: ', user_price)
-        with open('file_content/product.csv', 'a') as file:
-            fieldnames = ['Name', 'Price']
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writerow({'Name': user_new, 'Price': user_price})
-        file.close()
+        print('you created a new product - ', 'Product Name: ', user_new, ',' , 'Price: ', user_price)
+        sql = "INSERT INTO products (PRODUCT_NAME, PRICE) VALUES (%s, %s)"
+        val = (user_new, user_price)
+        self.cursor.execute(sql, val)
+        self.connection.commit()
+        print(self.cursor.rowcount, "record inserted.")
         print('Product Successfully Added!')
         p_menu.get_product_menu()
 
     def update_product(self):
         while True:
-            print('This are the list of products we have below: ')
-            p_menu.load_products()
-            print('If you want to update a product, ')
-            user_pro_num = int(input('Enter an index value of the product you want to update: '))       
-            if user_pro_num > len(self.products_list):
-                print('Out of range, Try again!')
-                continue
-
+            print('This are the list of products we have below: \n')
+            p_menu.get_products_from_sql()
+            print('If you want to update a product, \n')
+            user_pro_id = input('Enter the product id you want to update: ')
+            user_pro_name = input('Enter the name of the product you want to update: ')
+            user_pro_price = input('Enter the price of the product you want to update: ')
+            if user_pro_id == "":
+                print(' \n Your input for product id is blank, No updates recorded.')
+                break
+            elif user_pro_name == "":
+                print(' \n Your input for product name is blank, No updates recorded.')
+                break
+            elif user_pro_price == "":
+                print(' \n Your input for product price is blank, No updates recorded.')
+                break
             else:
-                print('Do you want to update this product - ', self.products_list[user_pro_num], '?')
-                answer = input("Enter 'y' for YES and 'n' for NO: " )
-                if answer == 'y':
-                    key_value = input("Enter value if 'NAME' or 'PRICE' you want to update: ")       
-                    if key_value in self.products_list[user_pro_num]:
-                        print('you want to update the product ',key_value, ' - ', (self.products_list[user_pro_num])[key_value])
-                        p_update = input('Please input the correct detail now: ')
-                        (self.products_list[user_pro_num])[key_value] = p_update
-                        print('The updated detail is: ', self.products_list[user_pro_num], '\n')
-                        print(self.products_list)              
-                        p_menu.save_products()
-                        print('\n Update Successful!')
-
-                        print('Do you want to update anything else? ')
-                        reply = input("Enter 'y' for YES and 'n' for NO: " )
-                        if reply == 'y':
-                           continue
-                        elif reply == 'n':
-                            print('OK! You have returned to the product menu')
-                            break
-                        else:
-                            print('your input is blank. nothing to update')
-                            break
-            
-                    else:
-                        print('your input is blank. nothing to update')
-                        break        
-                elif answer == 'n':
-                    print('OK! You have returned to the product menu')
-                    break
-                else:
-                    print('Invalid Input, Try again')
-                    break
-        p_menu.get_product_menu()           
+                print('You want to update product id -', user_pro_id, 'with new product name -', user_pro_name, 'and new price -', user_pro_price, '\n')
+                sql = "UPDATE products SET PRODUCT_NAME = %s, PRICE = %s WHERE PRODUCT_ID = %s"
+                val = (user_pro_name, user_pro_price, user_pro_id)
+                self.cursor.execute(sql, val)
+                self.connection.commit()
+                print(self.cursor.rowcount, "record(s) affected")
+                print('Details Updated Successfully!.')
+                break
+        p_menu.get_product_menu()
 
     def delete_product(self):
         while True:
-            print('The product list are: ')
-            p_menu.load_products()
-            print('you can choose the product to delete now!')
-            user_del = int(input('Enter an index value of the product you want to delete: '))
-            print('You selected - ', self.products_list[user_del])
-            del self.products_list[user_del]
-            print('\n Selected Product Deleted. \n The products remaining are: ')
-            print(self.products_list)
-            p_menu.save_products()
+            print('\n The product list are: \n')
+            p_menu.get_products_from_sql()
+            print('\n You can choose the product to delete now! \n')
+            user_del = input('Enter the product id you want to delete: \n')
+            print('You selected product id - ', user_del)
+            sql = "DELETE FROM products WHERE PRODUCT_ID = %s"
+            val = (user_del)
+            self.cursor.execute(sql, val)
+            self.connection.commit()
+            print(self.cursor.rowcount, "record(s) deleted")
             break
         p_menu.get_product_menu()
 
- 
+
 product_menu = ['MAIN MENU', 'PRODUCT LIST', 'CREATE', 'UPDATE', 'DELETE']
 p_menu = ProductMenu(product_menu)
 # p_menu.get_product_menu()
